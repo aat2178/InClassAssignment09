9d981bccb36a18f40af048d8213bf8f1cc2b64da
Azsanee T <aat2178@tc.columbia.edu>
2019-04-20 11:40:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/inclassassignment09/TestItem.java
@@ -0,0 +1,11 @@
+package com.example.inclassassignment09;
+
+public class TestItem {
+    String question;
+    String answer;
+
+    public TestItem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
